be4f6459fc181527c6b025dd8ad7bb0f5c7ba8a3
marcosdenisalves <marcos.denisjr@gmail.com>
2020-09-13 00:13:30 -0300

    Adicionando anotações na entidade conta

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,10 +1,15 @@
 package br.com.alura.jpa.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Conta {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	private Integer agencia;
 	private Integer numero;
