74953a6557f90067d09f3ad5a3a7b4a9d5bc59ba
marcosdenisalves <marcos.denisjr@gmail.com>
2020-09-19 01:01:30 -0300

    Criando MovimentacaoDAO

---
+++ src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDAO.java
@@ -0,0 +1,34 @@
+package br.com.alura.jpa.modelo.dao;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.MediaComData;
+
+public class MovimentacaoDAO {
+	private EntityManager em;
+	
+	public MovimentacaoDAO(EntityManager em) {
+		this.em = em;
+	}
+
+	public List<MediaComData> getMediaDiariaDasMovimentacoes() {
+		String jpql = "select new br.com.alura.modelo.MediaComData(avg(m.valor), "
+					+ "day(m.data), month(m.data)) from Movimentacao m "
+					+ "group by day(m.data), month(m.data), year(m.data)";
+
+		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
+		return query.getResultList();
+	}
+	
+	public BigDecimal getSomaDasMovimentacoes() {
+		String jpql = "select sum(m.valor) from Movimentacao m";
+		
+		TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
+		
+		return query.getSingleResult();
+	}
+}
